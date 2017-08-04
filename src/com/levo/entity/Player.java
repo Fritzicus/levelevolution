f331b6ead72633b4fe8abe8f52452c5e5a68e171
Josin Hu <josinh9154@gmail.com>
2017-08-04 15:49:36 +0200

    Polished the movement system

--- src/com/levo/entity/Player.java
+++ src/com/levo/entity/Player.java
@@ -1,157 +1,156 @@
 package com.levo.entity;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.event.KeyEvent;
-import java.util.List;
 
 import com.levo.game.Game;
-import com.levo.physics.AABB;
-import com.levo.physics.Collision;
 import com.levo.physics.Vec2;
 
-public class Player extends Entity {
-	// The Speed of the player moving on the X axis
-	public static final double SPEED = 2.5;
-	// Multiplier for speed boost when player is on the ground
-	public static final double GROUND_SPEED_MULTIPLIER = 1.5;
-	// Maximum speed in the Y axis for the player
-	public static final double MAX_Y_SPEED = 15;
-	// The Y-Velocity the player instantaneously obtains when jumping
-	public static final double JUMP_VELOCITY = -5.5;
+//to be removed
+import java.awt.Color;
+
+public final class Player extends Mobs {
+        // The accelleration of the player
+        public static final double NORMAL_SPEED = 5;
+        // The rate at which the player will slow down
+        public static final double NORMAL_DEACCEL = 0.125;
+        // The Y-Velocity the player accelerates by when jumping
+	public static final double JUMP_ACCEL = 6.5;
+        
+	// Maximum speed in the Y direction that the player can achieve
+	public static final double MAX_Y_VEL = 5;
+        // Maximum speed in the X direction that the player can achieve
+        public static final double MAX_X_VEL = 7.5;
+        
+        // Factor by which the NORMAL_ACCEL is multiplied when sprinting
+        public static final double SPRINT_FACTOR = 3;
 	// Wall jump movement cooldown to make sure player moves away from wall before going back to it
-	public static final int WALL_JUMP_COOLDOWN = 15;
+	public static final int WALL_JUMP_COOLDOWN = 5;
 	// Multiplier on gravity to be used during a wall slide to reduce slide speed
 	public static final double WALL_SLIDE_GRAVITY = .2;
 	
 	public static final int WIDTH = 15;
 	public static final int HEIGHT = 20;
-	public static final Color COLOR = new Color(102, 0, 150);
+        
 
-	// AABB representing the players position and size (Player is a rectangle)
-	private AABB aabb;
-	// Players velocity. Updated when keys are pressed, collisions occur, and with gravity
-	private Vec2 vel;
-	// Boolean representing whether or not the jump key is being pressed. Used track changes in jump key being pressed.
-	private boolean jumpKeyDown;
-	// True when player is actively walking into a wall while moving left
-	private boolean onLeftWall;
-	// True when player is actively walking into a wall while moving right
-	private boolean onRightWall;
-	// True when player is standing on something
-	private boolean onGround;
 	// Array with an index for every key code. keyDown[any key code] is true when that key is being pressed
-	private boolean[] keyDown;
-	// Integer representing the number of ticks the player cannot move left and right
-	private int noMoveCooldown;
+	private final boolean[] keyDown;
+        //Forces the player to move away from the wall
+        private int wallJumpCooldown;
 	
 	// Initialize with starting position. 
 	// Also passes a reference to keyDown array so when keyDown is updated in GameState.java, 
 	// the player keyDown array is effected
 	public Player(Vec2 pos, boolean[] keyDown) {
-		aabb = new AABB(pos, WIDTH, HEIGHT); 
-		vel = new Vec2(0, 0);
-		jumpKeyDown = false;
-		onLeftWall = false;
-		onRightWall = false;
-		onGround = false;
+		super(pos, WIDTH, HEIGHT, ID.PLAYER,5); 
 		this.keyDown = keyDown;
-		noMoveCooldown = 0;
 	}
 	
 	// Draws the player currently as a rectangle with a purple color
+        @Override
 	public void draw(Graphics2D g) {
-		aabb.draw(g, COLOR);
+		this.getAABB().draw(g, Color.MAGENTA);
 	}
 	
-	public void update() {
-		// Tests if the jump key is down and we haven't already recorded it. 
-		// This ensures that jump only triggers when jump key is initially pressed, not if held down
-		if (keyDown[KeyEvent.VK_SPACE] && !jumpKeyDown) {
-			if (onGround && vel.y == 0) {
-				// A regular jump is possible and will occur
-				vel.y = JUMP_VELOCITY;
-			} else if (onLeftWall) {
-				// A wall jump has happened on a wall to the left
-				vel.y = JUMP_VELOCITY;
-				vel.x = SPEED;
-				noMoveCooldown = WALL_JUMP_COOLDOWN; // A cooldown is necessary so the player moves away from the wall
-			} else if (onRightWall) {
-				// A wall jump has happened on a wall to the right
-				vel.y = JUMP_VELOCITY;
-				vel.x = -SPEED;
-				noMoveCooldown = WALL_JUMP_COOLDOWN;
-			}
+        public void deaccel() {
+        // Function that handles deacceleration to prevent the player from moving infinitely
+            if (Math.abs(this.getVel().x) != 0) {
+                //Deacceleration depends on the circumstances
+                if (this.getVel().x > 0) {
+                    this.getVel().x -= NORMAL_DEACCEL;
+                } else {
+                    this.getVel().x += NORMAL_DEACCEL;
+                }
+           
+            }
+        
+            if ((this.isOnLeftWall() || this.isOnRightWall()) && this.getVel().y >= 0) {
+                //Gravity is lessened by a factor of WALL_SLIDE_GRAVITY, to create a sliding effect
+                this.getVel().add(Game.GRAVITY.scaled(WALL_SLIDE_GRAVITY));
+            } else {
+                this.getVel().add(Game.GRAVITY);
+            }
+           
+            //Round to two decimal places
+            double rounded = Math.round(this.getVel().x*10);
+            this.getVel().x = rounded / 10;
+        }
+        
+        
+        public void jump() {
+        // Function that handles jumping
+            if (keyDown[KeyEvent.VK_SPACE]) {
+		if (this.isOnGround()) {
+                    // A regular jump is possible and will occur
+                    this.getVel().y -= JUMP_ACCEL;
 		} 
-		jumpKeyDown = keyDown[KeyEvent.VK_SPACE];
-				
-		// Allow player to move unless noMoveCooldown is positive, otherwise decrement it
-		if (noMoveCooldown <= 0) {
-			vel.x = 0;
-			// Check player movement keys and set speed accordingly
-			if (keyDown[KeyEvent.VK_A] || keyDown[KeyEvent.VK_LEFT]) {
-				vel.x = -SPEED;
-			} 
-			if (keyDown[KeyEvent.VK_D] || keyDown[KeyEvent.VK_RIGHT]) {
-				vel.x = SPEED;
-			}
+                if (this.isOnLeftWall() && wallJumpCooldown <= 0) {
+                    // A wall jump has happened on a wall to the left
+                    this.getVel().x = SPRINT_FACTOR*Math.abs(this.getVel().x);
+                    this.getVel().y -= JUMP_ACCEL;
+                    wallJumpCooldown = WALL_JUMP_COOLDOWN; // A cooldown is necessary so the player moves away from the wall
+		} 
+                else if (this.isOnRightWall() && wallJumpCooldown <= 0) {
+                    // A wall jump has happened on a wall to the right
+                    this.getVel().x = -SPRINT_FACTOR*Math.abs(this.getVel().x);
+                    this.getVel().y -= JUMP_ACCEL;
+                    wallJumpCooldown = WALL_JUMP_COOLDOWN;
 		} else {
-			noMoveCooldown--;
-		}
-		
-		// Apply ground movement speed bonus
-		if (onGround) 
-			vel.x *= GROUND_SPEED_MULTIPLIER;
-		
-		aabb.addVec(vel); // Move position by vel vector
-		
-		// If on wall, lower gravity to give a sliding down effect
-		if ((onLeftWall || onRightWall) && vel.y > 0)
-			vel.add(Game.GRAVITY.scaled(WALL_SLIDE_GRAVITY));
-		else
-			vel.add(Game.GRAVITY);
-		
-		// Keep y velocity within maximum bounds
-		if (vel.y > MAX_Y_SPEED) {
-			vel.y = MAX_Y_SPEED;
-		} else if (vel.y < -MAX_Y_SPEED) {
-			vel.y = -MAX_Y_SPEED;
-		}
+                    wallJumpCooldown--;
+                }  
+            } 
+        }
+        
+        public void walk() {
+            // Check player movement keys and set speed accordingly
+            if (keyDown[KeyEvent.VK_A] || keyDown[KeyEvent.VK_LEFT]) {
+                this.getVel().x = -NORMAL_SPEED;
+            }
+            if (keyDown[KeyEvent.VK_D] || keyDown[KeyEvent.VK_RIGHT]) {
+                this.getVel().x = NORMAL_SPEED;
+            }
+        }  
+        
+        
+        public void sprint () {
+                // Multiply the rate of acceleration by SPRINT_FACTOR is shift is held down
+
+                
+                if (this.isOnGround()) {
+                    if ((keyDown[KeyEvent.VK_A] || keyDown[KeyEvent.VK_LEFT]) && keyDown[KeyEvent.VK_SHIFT]) {
+                        this.getVel().x = -NORMAL_SPEED * SPRINT_FACTOR;
+                    }
+                    if ((keyDown[KeyEvent.VK_D] || keyDown[KeyEvent.VK_RIGHT]) && keyDown[KeyEvent.VK_SHIFT]) {
+                       this.getVel().x = NORMAL_SPEED * SPRINT_FACTOR;
+                    }
+                }
+        }
+        
+        @Override
+	public void update() {
+                
+                
+                if (MAX_X_VEL > Math.abs(this.getVel().x)) {                
+                    this.walk();
+                    this.sprint();
+                } else {
+                    if (this.getVel().x < 0) {
+                        this.getVel().x = -MAX_X_VEL;
+                    } else {
+                        this.getVel().x = MAX_X_VEL;
+                    }
+                }
+                this.jump();
+                this.deaccel();
+                this.getAABB().addVec(this.getVel()); // Move position by vel vector
+
 	}
 	
-	public void handleCollisions(List<Entity> blocks) {
-		// Make these checks false and set them to true they are detected
-		onGround = false;
-		onLeftWall = false;
-		onRightWall = false;
-		for (Entity e : blocks) {
-			Block b = (Block) e; // TODO check entity type before casting and doing special routines
-			// Check if block is colliding with player
-			if (b.getAABB().isColliding(aabb)) {
-				Collision c = b.getAABB().getCollision(aabb);
-				
-				// Check collision normal to know what direction collision occured in
-				if (c.getNormal().y == -1) {
-					vel.y = 0;
-					onGround = true;
-				} else if (c.getNormal().y == 1) {
-					vel.y = .1;
-				} else if (c.getNormal().x == 1) {
-					onLeftWall = true;
-				} else if (c.getNormal().x == -1) {
-					onRightWall = true;
-				}
-				
-				if (c.getDepth() >= 0) {
-					// Resolve collision by moving player back in the direction of the normal
-					aabb.addVec(c.getNormal().scaled(c.getDepth()));
-				}
-			}
-		}
-	}
+	@Override
+        public void attack() {
+            // TODO
+            // Check entity type
+            // do damage
+        }
 	
-	// Returns players center, useful for camera positioning
-	public Vec2 centerPoint() {
-		return aabb.centerPoint();
-	}
 }
