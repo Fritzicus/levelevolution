bc1f1beab35c090a045321338584d2d6a6b1432f
Josin Hu <josinh9154@gmail.com>
2017-08-07 15:08:28 +0200

    Updated Block.java

--- src/com/levo/entity/Block.java
+++ src/com/levo/entity/Block.java
@@ -3,30 +3,22 @@ package com.levo.entity;
 import java.awt.Color;
 import java.awt.Graphics2D;
 
-import com.levo.physics.AABB;
 import com.levo.physics.Vec2;
 
 // Represents a solid block that the player can jump and move around on
-// Currently is just an entity wrapping an AABB, but may have more functionality later
 public class Block extends Entity {
-
-	private AABB aabb;
-	private Color color;
-	
+    
+	private final Color color;
+        
 	public Block(Vec2 pos, int w, int h, Color color) {
-		aabb = new AABB(pos, w, h);
+                //Creates a new AABB object using the constructor of the superclass
+		super(pos, w, h, ID.BLOCK);
 		this.color = color;
 	}
 	
+        @Override
 	public void draw(Graphics2D g) {
-		aabb.draw(g, color);
+		this.getAABB().draw(g, color);
 	}
-	
-	public AABB getAABB() {
-		return aabb;
-	}
-	
-	public Vec2 centerPoint() {
-		return aabb.centerPoint();
-	}
+		
 }
