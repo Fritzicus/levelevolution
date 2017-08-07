2abefe021b29a57854e306b46b876fc7dfc06ceb
Josin Hu <josinh9154@gmail.com>
2017-08-07 15:04:08 +0200

    Updated Entity.java

--- src/com/levo/entity/Entity.java
+++ src/com/levo/entity/Entity.java
@@ -3,14 +3,39 @@ package com.levo.entity;
 import java.awt.Graphics2D;
 
 import com.levo.physics.Vec2;
+import com.levo.physics.AABB;
 
 // Top level entity class for anything which is displayed on screen and interacts with other entities (i.e. Player, Blocks, Enemies, Particles, etc.)
 public abstract class Entity {
 
-	// TODO, find things to be abstracted into Entity class (physics? sprite?) If nothing is found, remove entity class
+        //Each entity's coordinates and size are represented by this AABB
+	private final AABB aabb;
+        //The type of the entity as an ID
+        private ID type;
 	
-	public abstract void draw(Graphics2D g);
-	public void update() {	}
-	
-	public abstract Vec2 centerPoint();
+        //Constructor is only accessible by its subclasses
+        protected Entity (Vec2 pos, int w, int h, ID inType) {
+            this.aabb = new AABB(pos, w, h);
+            this.type = inType;
+        }
+        
+        public abstract void draw(Graphics2D g);
+        
+	//Returns the centerpoint of the enitty
+        public Vec2 centerPoint() {
+		return this.aabb.centerPoint();
+	}
+        
+        public AABB getAABB() {
+		return this.aabb;
+	}
+        
+	public ID getID(){
+            return this.type;
+        }
+        
+        public void setID(ID inType) {
+            this.type = inType;
+        }
+        
 }
