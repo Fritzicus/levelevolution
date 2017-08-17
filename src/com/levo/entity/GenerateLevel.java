194da836423e27bb3b666efeee11d8b3c98d52e0
Fritzicus <butler273@hotmail.com>
2017-08-17 11:37:54 -0700

    Added Room Clustering
    Added room clustering. Next step is to connect the closest proximity of clusters together.

--- src/com/levo/entity/GenerateLevel.java
+++ src/com/levo/entity/GenerateLevel.java
@@ -32,75 +32,121 @@ public class GenerateLevel {
 		int errors = 0;
 		int roomID = 1;
 		int blockScaleFactor = 5;
-		int [][]overlayMap = new int[levelX][levelY]; 
+		int[][] overlayMap = new int[levelX][levelY];
 		
-		// ArrayList<Room> roomList = new ArrayList<Room>();
+		ArrayList<Integer> adjacentRooms = new ArrayList<Integer>();
+		ArrayList<Integer> simplifyRooms = new ArrayList<Integer>();
+		
+
 		ArrayList<ArrayList<Room>> roomList = new ArrayList<ArrayList<Room>>();
-		
+
 		for (int i = 0; i < levelX; i++) {
 			for (int j = 0; j < levelY; j++) {
 				levelMap[i][j] = 1;
-				overlayMap[i][j]= 0;
+				overlayMap[i][j] = 0;
 			}
 		}
-		
+
 		while (roomsCompleted < maxNumberOfRooms) {
-			int sizeX = rand.nextInt(5) + 3;
+			int sizeX = rand.nextInt(5) + 3; // Generates new room dimensions and placement at random
 			int sizeY = rand.nextInt(3) + 1;
 			int posX = rand.nextInt(levelX - 9) + 1;
 			int posY = rand.nextInt(levelX - 5) + 1;
-			int maxCluster = 0;
-			Room newRoom = new Room(posX, posY,sizeX, sizeY, roomID, 0);
 			int maxOverlap = 0;
-			if(!roomList.isEmpty())
-			System.out.println(roomList.get(0).get(0).getRoomID());
-			for(ArrayList<Room> i : roomList ) {
-				for(Room j : i) {
-				int currentOverlap = j.calculateOverlap(newRoom);
-					if(currentOverlap == -1){
+			Room newRoom = new Room(posX, posY, sizeX, sizeY, roomID, 0); // instantiates a new room object
+			adjacentRooms.clear();
+			simplifyRooms.clear();
+			//System.out.println("For room: " + roomID);
+			
+			for (ArrayList<Room> i : roomList) {
+				for (Room j : i) {                                        // For loops checks to see if rooms are not overlapping
+					int currentOverlap = j.calculateOverlap(newRoom);
+					if (currentOverlap == -1) 
 						maxOverlap = -1;
-					}
+					if (maxOverlap != -1) 
+						if(currentOverlap !=0)
+							adjacentRooms.add(currentOverlap);
 				}
+
 			}
-			if(maxOverlap > -1.0 ) {
-				roomsCompleted++;
-				boolean newCluster = true;
-				for(ArrayList<Room> i : roomList ) {
-					for(Room j : i) {
-						if(j.getRoomID() == maxOverlap) {
-							i.add(newRoom);
-							newCluster = false;
-						}
-					}
-				}
-				if(newCluster == true ) {
-					roomList.add(new ArrayList<Room>());
-					roomList.get(maxCluster).add(newRoom);
-					maxCluster++;
-				}
-				roomID++;
-				
+			if (maxOverlap > -1) {
 				for (int i = 0; i < sizeX; i++) {
 					for (int j = 0; j < sizeY; j++) {
-						levelMap[i+posX][j+posY]=0;
-						overlayMap[i+posX][j+posY]=roomID;
+						levelMap[i + posX][j + posY] = 0;
+						overlayMap[i + posX][j + posY] = roomID;
 					}
 				}
+				if (adjacentRooms.isEmpty()) { // if there is not an adjacent room, add new room cluster to arraylist
+					roomList.add(new ArrayList<Room>());
+					roomList.get(roomList.size()-1).add(newRoom);
+				}else 
+					for (int i = 0; i < roomList.size(); i++) 
+						for (int j = 0; j < roomList.get(i).size(); j++) 
+							for(int k : adjacentRooms) 
+								if(roomList.get(i).get(j).getRoomID() == k) // checks to see if there are adjacent rooms
+									if(!simplifyRooms.contains(i))
+										simplifyRooms.add(i);
+
+				if(!simplifyRooms.isEmpty()) {
+					roomList.get(simplifyRooms.get(0)).add(newRoom);
+				}
 				
-			}else {
+				if(simplifyRooms.size() > 1) {
+					for(int i = 1; i<simplifyRooms.size();i++) {
+						roomList.get(simplifyRooms.get(0)).addAll(roomList.get(simplifyRooms.get(i)));
+						roomList.get(simplifyRooms.get(i)).clear();
+					}
+
+				}
+				System.out.println("for RoomID: "+ roomID);
+				for(int i : adjacentRooms)
+					System.out.print(i + " ");
+				
+				System.out.println("");
+				for(int i : simplifyRooms)
+					System.out.print(i + " ");
+				System.out.println("");
+				adjacentRooms.clear();
+				simplifyRooms.clear();
+				roomsCompleted++;
+				roomID++;
+
+				
+				/*for (ArrayList<Room> i : roomList) {
+					for (Room j : i) {
+						System.out.print(j.getRoomID()+" ");
+					}
+					System.out.println("");
+				}
+				*/
+			} else {
 				errors++;
 			}
-			if(errors > 100000) { // prevents the room generation from potentially getting stuck in an infinite loop.
+			if (errors > 100) { // prevents the room generation from potentially getting stuck in an infinite
+									// loop.
 				errors = 0;
 				System.out.println("Lots of errors");
 				roomsCompleted++;
 			}
-			// System.out.println(maxOverlap);
-			maxOverlap = 0;
 		}
+
+			
 		/// this segment is intended to simplify geometry to single blocks instead of
 		/// multiple blocks
-
+		/*
+		for (ArrayList<Room> i : roomList) {
+			for (Room j : i) {
+				System.out.print(j.getRoomID()+" ");
+			}
+			System.out.println("");
+		}
+		*/
+		for (ArrayList<Room> i : roomList) {
+			for (Room j : i) {
+				System.out.print(j.getRoomID()+" ");
+			}
+			System.out.println("");
+		}
 		
 		for (int i = 0; i < levelX; i++) {
 			for (int j = 0; j < levelY; j++) {
@@ -109,35 +155,14 @@ public class GenerateLevel {
 			}
 			System.out.println("");
 		}
-		
-		/*
 
 		for (int i = 0; i < levelX; i++) {
-			int adjacency = 0;
 			for (int j = 0; j < levelY; j++) {
 				if (levelMap[i][j] == 1)
-					adjacency++;
-				else {
-					if (adjacency != 0)
-						levelGrid.add(new Block(new Vec2(i * BlockScaleFactor, (j - adjacency) * BlockScaleFactor),
-								BlockScaleFactor, BlockScaleFactor * (adjacency), Color.GREEN));
-					adjacency = 0;
-				}
+					levelGrid.add(new Block(new Vec2(i * blockScaleFactor, j * blockScaleFactor), blockScaleFactor,
+							blockScaleFactor, Color.GREEN));
 			}
-			if (adjacency > 1)
-				levelGrid.add(new Block(new Vec2(i * BlockScaleFactor, 0), BlockScaleFactor,
-						BlockScaleFactor * (adjacency + 1), Color.GREEN));
-			adjacency = 0;
 		}
-        */
-		// the original block implementation
-		
-		 for(int i = 0 ; i<levelX; i++) { 
-			 for(int j = 0; j <levelY; j++ ) {
-				 if(levelMap[i][j]==1) 
-					 levelGrid.add(new Block(new Vec2(i*blockScaleFactor, j*blockScaleFactor), blockScaleFactor, blockScaleFactor, Color.GREEN)); 
-			 }
-		 }
 		return levelGrid;
 	}
 }
--- src/com/levo/entity/Room.java
+++ src/com/levo/entity/Room.java
@@ -25,57 +25,28 @@ public class Room {
 		
 		int intersectPosX= Math.max(this.posX, compare.posX);
 		int intersectPosY= Math.max(this.posY, compare.posY);
-		int xHighest= Math.max((this.posX + this.sizeX - 1), (compare.posX + compare.sizeX -1));
-		int yHighest= Math.max((this.posY + this.sizeY - 1), (compare.posY + compare.sizeY -1));
-		
-
+		int xRight  = Math.min((this.posX + this.sizeX-1), (compare.posX + compare.sizeX-1));
+		int yBottom = Math.min((this.posY + this.sizeY-1), (compare.posY + compare.sizeY-1));
+ 		int xHighest= Math.max((this.posX + this.sizeX - 1), (compare.posX + compare.sizeX -1));
+ 		int yHighest= Math.max((this.posY + this.sizeY - 1), (compare.posY + compare.sizeY -1));
+	    int intersectSizeX = Math.max(0, xRight - intersectPosX + 1);
+	    int intersectSizeY = Math.max(0, yBottom - intersectPosY + 1);
 	    
-	    int intersectSizeX = Math.max(0, (Math.min((this.posX + this.sizeX-1), (compare.posX + compare.sizeX-1))) - intersectPosX + 1);
-	    int intersectSizeY = Math.max(0, (Math.min((this.posY + this.sizeY-1), (compare.posY + compare.sizeY-1))) - intersectPosY + 1);
 	    
-	    // System.out.println(  (this.posX + this.sizeX) + " " +  + (compare.posX + compare.sizeX) + " " +intersectPosX + " " + intersectSizeX + " " + intersectSizeY);
-	    // System.out.println(  (this.posX + this.sizeX) + " " +  intersectSizeX + " " + intersectSizeY);
-	    
-	    if(intersectPosX > xHighest){
-	    	if(intersectPosX > xHighest + 1)
-	    		return -1;
-	    	else if(intersectSizeY>0){
+	    if(intersectSizeX > 0 && intersectSizeY > 0)
+	    	return -1;
+	    if(intersectSizeX < 1 && intersectSizeY < 1)
+	    	return 0; 
+	     if(intersectSizeX > 0) 
+	    	if(intersectPosY == yBottom + 1)
 	    		return this.roomID;
-	    	}else {
-	    		return -1;
-	    	}
-	    }
-	    if(intersectPosY > yHighest){
-	    	if(intersectPosY > yHighest + 1)
+	    	else
 	    		return 0;
-	    	else if(intersectSizeX>0){
+	    else 
+	    	if(intersectPosX == xRight + 1)
 	    		return this.roomID;
-	    	}else {
-	    		return -1;
-	    	}
-	    }
-	    
-	    if(intersectSizeX < 1 || intersectSizeY < 1) // return 0 if there is no intersect  area
-	    	return 0;	
-	   
-	    
-	    /*
-	    double totalIntersectArea = (double) (intersectSizeX * intersectSizeY);
-	    double totalArea = (double) (sizeX * sizeY);
-	   
-	    double compareTotalArea = (double) (double) (compare.sizeX * compare.sizeY);
-		/*
-	    if(totalArea < compareTotalArea ) {
-	    	//System.out.println(totalArea + " " + totalIntersectArea);
-	    	return (totalArea / totalIntersectArea);
-	    }else {
-	    	//System.out.println(compareTotalArea + " " + totalIntersectArea);
-	    	return (compareTotalArea / totalIntersectArea);
-	    }
-		
-		*/
-	    return -1;
-		
-		
+	    	else
+	    		return 0;
+
 	}
 }
